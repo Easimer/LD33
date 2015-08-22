816394b8b586aadfdf305b2e96fe766cb7b6365e
Easimer <easimer@gmail.com>
2015-08-22 11:31:40 +0200

    Change function declaration style, add callbacks
    entities.lua:
    * Change function style from var = f() to f var()
    * Add mousep, mouser, keyp, keyr callbacks

--- entities.lua
+++ entities.lua
@@ -4,33 +4,37 @@ entities._entlist = {}
 entities._lastid = 0
 
 --Create new entry in entities and return the ID
-entities.create_entity = function()
+function entities.create_entity()
   entities._lastid = entities._lastid + 1
   newent = {}
   newent.id = entities._lastid
   newent.update = function(dt) end
   newent.draw = function() end
+  newent.mousepressed = function(x,y,b) end
+  newent.mousereleased = function(x,y,b) end
+  newent.keypressed = function(k) end
+  newent.keyreleased = function(k) end
   table.insert(entities._entlist, entities._lastid, newent)
   print("New entitity " .. tostring(newent) .. " created with ID " .. newent.id)
   return newent.id, newent
 end
 
 --Call update on every entity
-entities.update = function(dt)
+function entities.update(dt)
   for k,v in pairs(entities._entlist) do
     v.update(dt)
   end
 end
 
 --Call draw on every entity
-entities.draw = function()
+function entities.draw()
   for k,v in pairs(entities._entlist) do
     v.draw()
   end
 end
 
 --Return entity with ID `id`
-entities.get_entity = function(id)
+function entities.get_entity(id)
   if entities._entlist[id] == nil then
     return nil
   end
@@ -39,4 +43,32 @@ end
 
 entities.destroy_entity = function(id)
   entities._entlist.remove()
+end
+
+--Call mousepressed on every entity
+function entities.mousepressed(x, y, b)
+  for k,v in pairs(entities._entlist) do
+    v.mousepressed(x,y,b)
+  end
+end
+
+--Call mousereleased on every entity
+function entities.mousereleased(x, y, b)
+  for k,v in pairs(entities._entlist) do
+    v.mousereleased(x,y,b)
+  end
+end
+
+--Call keypressed on every entity
+function entities.keypressed(key)
+  for k,v in pairs(entities._entlist) do
+    v.keypressed(key)
+  end
+end
+
+--Call keyreleased on every entity
+function entities.keyreleased(key)
+  for k,v in pairs(entities._entlist) do
+    v.keyreleased(key)
+  end
 end
--- main.lua
+++ main.lua
@@ -10,3 +10,19 @@ end
 function love.draw()
   entities.draw()
 end
+
+function love.mousepressed(x, y, button)
+  entities.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+  entities.mousereleased(x, y, button)
+end
+
+function love.keypressed(key)
+  entities.keypressed(key)
+end
+
+function love.keyreleased(key)
+  entities.keyreleased(key)
+end
