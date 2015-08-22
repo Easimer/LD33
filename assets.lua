d83f92c6e0dd346479cc983d85bb18d8321d7e0c
Easimer <easimer@gmail.com>
2015-08-22 12:28:14 +0200

    Add Asset Manager
    assets.lua:
    * Add asset manager

---
+++ assets.lua
@@ -0,0 +1,41 @@
+assets = {}
+
+assets._assets = {}
+
+--Load graphics asset
+function assets.load_image(fn, name)
+  img = love.graphics.newImage(fn)
+  assets._assets[name] = img
+  return assets._assets[name]
+end
+
+--Load font asset
+function assets.load_font(fn, name)
+  fnt = love.graphics.newFont(fn)
+  assets._assets[name] = fnt
+  return assets._assets[name]
+end
+
+--Load static sound asset
+function assets.load_staticsound(fn, name)
+  snd = love.audio.newSource(fn, "static")
+  assets._assets[name] = snd
+  return assets._assets[name]
+end
+
+--Load streaming sound asset
+function assets.load_streamsound(fn, name)
+  snd = love.audio.newSource(fn, "stream")
+  assets._assets[name] = snd
+  return assets._assets[name]
+end
+
+--Get asset by name
+function assets.get(name)
+  return assets._assets[name]
+end
+
+--Unload asset called `name`
+function assets.unload(name)
+  assets._assets[name] = nil
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require "assets"
 require "entities"
 
 function love.load()
