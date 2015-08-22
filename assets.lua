962afe5ecac32727035d82932ae568f29a497a2f
Easimer <easimer@gmail.com>
2015-08-22 14:46:36 +0200

    assets class now stores assets by filename
    assets.lua:
    * Now stores files by filename

--- assets.lua
+++ assets.lua
@@ -3,31 +3,31 @@ assets = {}
 assets._assets = {}
 
 --Load graphics asset
-function assets.load_image(fn, name)
+function assets.load_image(fn)
   img = love.graphics.newImage(fn)
-  assets._assets[name] = img
-  return assets._assets[name]
+  assets._assets[fn] = img
+  return assets._assets[fn]
 end
 
 --Load font asset
-function assets.load_font(fn, name)
+function assets.load_font(fn)
   fnt = love.graphics.newFont(fn)
-  assets._assets[name] = fnt
-  return assets._assets[name]
+  assets._assets[fn] = fnt
+  return assets._assets[fn]
 end
 
 --Load static sound asset
-function assets.load_staticsound(fn, name)
+function assets.load_staticsound(fn)
   snd = love.audio.newSource(fn, "static")
-  assets._assets[name] = snd
-  return assets._assets[name]
+  assets._assets[fn] = snd
+  return assets._assets[fn]
 end
 
 --Load streaming sound asset
-function assets.load_streamsound(fn, name)
+function assets.load_streamsound(fn)
   snd = love.audio.newSource(fn, "stream")
-  assets._assets[name] = snd
-  return assets._assets[name]
+  assets._assets[fn] = snd
+  return assets._assets[fn]
 end
 
 --Get asset by name
