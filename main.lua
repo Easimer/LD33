212f49fe62161b5f7a1734b5d46e5af7cb3b5c05
Easimer <easimer@gmail.com>
2015-08-23 02:14:13 +0200

    Move game.lua to vectordecay.lua, add loadGame
    game.lua -> vectordecay.lua
    
    main.lua:
    * Add loadGame method

--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
-require "game"
+function loadGame(gamemode)
+  require(gamemode)
+  --Load game
+  love.load = game.load or function() end
+  love.update = game.update or function(dt) end
+  love.draw = game.draw or function() end
+  love.mousepressed = game.mousepressed or function(x,y,b) end
+  love.mousereleased = game.mousereleased or function(x,y,b) end
+  love.keypressed = game.keypressed or function(k) end
+  love.keyreleased = game.keyreleased or function(k) end
+  love.focus = game.focus or function(f) end
+  love.mousefocus = game.mousefocus or function(f) end
+  love.mousemoved = game.mousemoved or function(x,y,dx,dy) end
+  love.resize = game.resize or function(x,y) end
+  love.quit = game.quit or function() end
+  love.textinput = game.textinput or function() end
+  love.visible = game.visible or function(v) end
+  love.threaderror = game.threaderror or function(t,errstr) end
+  love.errhand = game.errhand or love.errhand
+  print("init ok")
+end
 
---Load game
-love.load = game.load or function() end
-love.update = game.update or function(dt) end
-love.draw = game.draw or function() end
-love.mousepressed = game.mousepressed or function(x,y,b) end
-love.mousereleased = game.mousereleased or function(x,y,b) end
-love.keypressed = game.keypressed or function(k) end
-love.keyreleased = game.keyreleased or function(k) end
-love.focus = game.focus or function(f) end
-love.mousefocus = game.mousefocus or function(f) end
-love.mousemoved = game.mousemoved or function(x,y,dx,dy) end
-love.resize = game.resize or function(x,y) end
-love.quit = game.quit or function() end
-love.textinput = game.textinput or function() end
-love.visible = game.visible or function(v) end
-love.threaderror = game.threaderror or function(t,errstr) end
-love.errhand = game.errhand or love.errhand
-print("init ok")
+loadGame("vectordecay")
