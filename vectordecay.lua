ca6fb026abe9ac1c3bac36f619999d2ce7a485ca
Easimer <easimer@gmail.com>
2015-09-26 18:55:15 +0200

    Add layers, reset version
    vectordecay.lua:
    * Add drawing layers
    * Reset version

--- vectordecay.lua
+++ vectordecay.lua
@@ -7,7 +7,7 @@ require "cell"
 require "sound"
 
 game = {
-  version = "0,0,0,7.2,1,0,0.3,0,0,0#2",
+  version = "0.1.0",
   ticktime = 0,
   player = nil,
   camera = {
@@ -15,6 +15,16 @@ game = {
     y = 0
   },
   background = nil,
+  layers = {
+    game = {
+      canvas = nil,
+      shader = nil
+    },
+    gui = {
+      canvas = nil,
+      shader = nil
+    }
+  }
 }
 
 function game.load()
@@ -26,6 +36,9 @@ function game.load()
   p2.pos = {x = 0, y = 0}
   game.background = entities.add_entity(background)
   entities.add_entity(class(cell)).pos = {x = 25, y = 300}
+  game.layers.game.canvas = love.graphics.newCanvas()
+  --game.layers.gui.canvas = love.graphics.newCanvas()
+  --game.layers.game.shader = love.graphics.newShader("data/ca.fs")
 end
 
 function game.update(dt)
@@ -46,8 +59,33 @@ function game.update(dt)
 end
 
 function game.draw()
+  --First draw things to Game layer
+  game.layers.game.canvas:clear()
+  love.graphics.setCanvas(game.layers.game.canvas)
   game.background:draw()
   entities.draw()
+  love.graphics.setCanvas()
+  --Then draw GUI elements to the GUI layer
+  --[[
+  game.layers.gui.canvas:clear()
+  love.graphics.setCanvas(game.layers.gui.canvas)
+  gui_sys.draw()
+  love.graphics.setCanvas()
+  ]]
+  if game.layers.game.canvas then
+    if game.layers.game.shader then
+      love.graphics.setShader(game.layers.game.shader)
+    end
+    love.graphics.draw(game.layers.game.canvas)
+    love.graphics.setShader()
+  end
+  if game.layers.gui.canvas then
+    if game.layers.gui.shader then
+      love.graphics.setShader(game.layers.gui.shader)
+    end
+    love.graphics.draw(game.layers.gui.canvas)
+    love.graphics.setShader()
+  end
 end
 
 function game.mousepressed(x, y, button)
@@ -140,13 +178,22 @@ function game.errhand(msg)
 	p = string.gsub(p, "\t", "")
 	p = string.gsub(p, "%[string \"(.-)\"%]", "%1")
   local logofont = love.graphics.newFont(math.floor(love.window.toPixels(24)))
+  local panic = false
+  local t = 0
 	local function draw()
 		local pos = love.window.toPixels(100)
 		love.graphics.clear()
+    love.graphics.setColor(255, 255, 255)
     love.graphics.setFont(logofont)
     love.graphics.print("Vector Decay v" .. game.version, love.window.toPixels(95), love.window.toPixels(38))
     love.graphics.setFont(font)
 		love.graphics.printf(p, pos, pos, love.graphics.getWidth() - pos)
+    if panic then
+      love.graphics.setColor(200, 255, 0)
+      love.graphics.point(5, 5)
+      love.graphics.point(15, 5)
+      love.graphics.point(25, 5)
+    end
 		love.graphics.present()
 	end
 	while true do
@@ -162,6 +209,11 @@ function game.errhand(msg)
 		draw()
 		if love.timer then
 			love.timer.sleep(0.1)
+      t = t + love.timer.getDelta()
+      if t > 0.25 then
+        panic = not panic
+        t = 0
+      end
 		end
 	end
 end
